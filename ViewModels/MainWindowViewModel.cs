89728de634195e6bd68296eec6aff605038ddb43
pavels1982@mail.ru <pavels1982@mail.ru>
2020-07-20 21:46:37 +0300

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Requester.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Commands
+        public ICommand CloseWindow
+        {
+            get 
+            {
+                return new RelayCommand((o) => CloseApp());
+            }
+        }
+        #endregion
+
+
+        #region Constructor
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public MainWindowViewModel()
+        { 
+        }
+        #endregion
+
+
+        #region Methods
+        private void CloseApp()
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+    }
+}
---
+++ Views/MainWindowView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Requester.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Requester.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void HideToTray(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+    }
+}
